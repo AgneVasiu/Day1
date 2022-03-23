9706de84c6bf48434e78b687b30ba063757682d6
AgneVasiu <agne.vasiukeviciute@stud.viko.lt>
2022-03-24 00:05:49 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int packets = 5;
+	double price = 5.30;
+	char letter = 'D';
+	boolean is_true = true;
+
+		System.out.println("Packets ordered " + packets);
+		System.out.println("Price is " + price);
+		System.out.println("Letter is " + letter);
+		System.out.println("Is true = " + is_true);
+    }
+}
